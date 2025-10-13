894c3ecc95a86df2f73ce2cc42297f3afc5f7d19
Pavel <pavlopavlo2005500@gmail.com>
2025-10-13 12:46:14 +0300

    feat(ui): implement section switching logic

--- mvc/controller.py
+++ mvc/controller.py
@@ -2,3 +2,11 @@ class Controller:
     def __init__(self, model, view):
         self.model = model
         self.view = view
+
+        # Обработчики
+        self.view.tab_button_clicked(self.on_tab_button_clicked) # Нажатие на кнопку раздела
+
+    def on_tab_button_clicked(self, button):
+        """Функция обрабатывает нажатие на кнопку раздела"""
+        page = self.view.get_tab_page(button)
+        self.view.set_tab_page(page)
--- mvc/view.py
+++ mvc/view.py
@@ -1,7 +1,7 @@
 from resources import resources_rc
 
 from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QLineEdit, QPushButton
+from PyQt5.QtWidgets import QLineEdit
 
 
 class View:
@@ -14,8 +14,28 @@ class View:
 
         # Иконки для кнопок разделов (ПО УМОЛЧАНИЮ)
         download_icon = QIcon(":/icons/download_tab_white.svg")
-        add_icon = QIcon(":/icons/add_tab_blue.svg")
-        delete_icon = QIcon(":/icons/delete_tab_blue.svg")
+        add_icon = QIcon(":/icons/add_tab_white.svg")
+        delete_icon = QIcon(":/icons/delete_tab_white.svg")
         self.ui.download_tab_pushButton.setIcon(download_icon)
         self.ui.add_tab_pushButton.setIcon(add_icon)
         self.ui.delete_tab_pushButton.setIcon(delete_icon)
+
+        # Словарь кнопок разделов, где ключ - кнопка, значение - раздел
+        self.tabs_dict = {
+            self.ui.download_tab_pushButton: self.ui.download_page,
+            self.ui.add_tab_pushButton: self.ui.add_page,
+            self.ui.delete_tab_pushButton: self.ui.delete_page
+        }
+
+    def get_tab_page(self, button):
+        """Функция возвращает страницу для переданной кнопки"""
+        return self.tabs_dict[button]
+    
+    def set_tab_page(self, page):
+        """Функция устанавливает страницу для отображения"""
+        self.ui.tabs_stackedWidget.setCurrentWidget(page)
+
+    def tab_button_clicked(self, handler):
+        """Функция устанавливает обработчик нажатия на кнопку раздела"""
+        for button in self.tabs_dict.keys():
+            button.clicked.connect(lambda _, btn=button: handler(btn))
